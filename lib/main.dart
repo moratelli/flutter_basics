f7d6408fee15cb09a5e69e35471d83dee055078c
Pedro <pedromoratelli@gmail.com>
2020-09-05 01:03:40 -0300

    Update widget data

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex++;
+    print(questionIndex);
+  }
+
+  var questions = [
+    'What\'s is your favourite color?',
+    'What\'s your favourite animal?'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,7 +22,27 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text!'),
+        body: Column(
+          children: [
+            Text(
+              questions[questionIndex],
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                print('Answer 3 chosen!');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
