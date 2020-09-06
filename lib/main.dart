d6cd8a66e6ef410dbfe0a1ec1f25099b236ce835
Pedro <pedromoratelli@gmail.com>
2020-09-06 01:52:18 -0300

    Add score system, Add score message to result

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
+import './quiz.dart';
 import './answer.dart';
+import './result.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,48 +14,74 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  var _questionIndex = 0;
+  final _questions = const [
+    {
+      'questionText': 'What\'s is your favourite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1}
+      ]
+    },
+    {
+      'questionText': 'What\'s your favourite animal?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 10},
+        {'text': 'Dog', 'score': 1},
+        {'text': 'Cat', 'score': 1},
+        {'text': 'Bird', 'score': 5}
+      ]
+    },
+    {
+      'questionText': 'What\'s your favourite language?',
+      'answers': [
+        {'text': 'Dart', 'score': 1},
+        {'text': 'JavaScript', 'score': 1},
+        {'text': 'Python', 'score': 1},
+        {'text': 'Kotlin', 'score': 1}
+      ]
+    },
+  ];
 
-  void _answerQuestion() {
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
     setState(() {
       _questionIndex++;
     });
     print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print('We have more _questions!');
+    } else {
+      print('No more _questions!');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      {
-        'questionText': 'What\'s is your favourite color?',
-        'answers': ['Black', 'Red', 'Green', 'White']
-      },
-      {
-        'questionText': 'What\'s your favourite animal?',
-        'answers': ['Rabbit', 'Dog', 'Cat', 'Bird']
-      },
-      {
-        'questionText': 'What\'s your favourite language?',
-        'answers': ['Dart', 'JavaScript', 'Python', 'Kotlin']
-      },
-    ];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: [
-            Question(
-              questions[_questionIndex]['questionText'],
-            ),
-            ...(questions[_questionIndex]['answers'] as List<String>)
-                .map((answer) {
-              return Answer(_answerQuestion, answer);
-            }).toList()
-          ],
-        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
       ),
     );
   }
